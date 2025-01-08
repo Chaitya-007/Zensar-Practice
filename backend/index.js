4cc8612c692045fb37f03a5b4f0650ff867cf5c9
Chaitanya <chaitanyaasole@gmail.com>
2025-01-08 16:12:22 +0530

    making server from backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config({});
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGODB_URL;
+
+// 1st dimension is route, 2nd is callback function
+// to get resource from server
+app.get("/", (request, response) => {
+  console.log("Hello from server");
+  return response.status(234).send("Hello from server");
+});
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // 1st dimension is port, 2nd is callback function
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+  });
