434dc17338a9b61f9e6745eb4d2b8dac7e21eda2
Chaitanya <chaitanyaasole@gmail.com>
2025-01-08 16:40:58 +0530

    working on post method

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { BookModel } from "./models/bookModel.js";
 
 const app = express();
 dotenv.config({});
@@ -12,6 +13,33 @@ const MONGO_URI = process.env.MONGODB_URL;
 app.get("/", (request, response) => {
   console.log("Hello from server");
   return response.status(234).send("Hello from server");
+});
+
+// route to create new book
+app.post("/", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response
+        .status(400)
+        .send({ message: "Please fill all required fields" });
+    }
+
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await BookModel.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
