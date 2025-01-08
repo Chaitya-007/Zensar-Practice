432bd976e003ea55ced73bd98abf05720240d4e6
Chaitanya <chaitanyaasole@gmail.com>
2025-01-08 20:41:26 +0530

    use default cors

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,14 @@ const MONGO_URI = process.env.MONGODB_URL;
 
 app.use(express.json());
 
-// app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 // 1st dimension is route, 2nd is callback function
 // to get resource from server
