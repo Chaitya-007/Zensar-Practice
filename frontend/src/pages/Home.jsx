4185ab1d64b1f81285654d439c25db92812c868e
Chaitanya <chaitanyaasole@gmail.com>
2025-01-08 21:36:14 +0530

    did with show book and back button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader.jsx";
+import axios from "axios";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get("http://localhost:5555/books")
+      .then((res) => {
+        setBooks(res.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      Books List
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Publish Year</th>
+              <th>Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.publishYear}</td>
+                <td>
+                  <div>
+                    <Link to={`/books/details/${book._id}`}>Deatils</Link>
+                    <Link to={`/books/edit/${book._id}`}>Edit</Link>
+                    <Link to={`/books/delete/${book._id}`}>Delete</Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton.jsx";
+import Loader from "../components/Loader.jsx";
 
 const ShowBook = () => {
-  return <div>ShowBook</div>;
+  const [book, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setBooks(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <BackButton />
+      <h1>Show Book</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <h3>id : {book._id}</h3>
+          <h3>Title : {book.title}</h3>
+          <h3>Author : {book.author}</h3>
+          <h3>Publish Year : {book.publishYear}</h3>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ShowBook;
