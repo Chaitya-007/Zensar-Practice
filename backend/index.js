d16843df2d7c8a4113b13cb097c5b58697f1d0cd
Chaitanya <chaitanyaasole@gmail.com>
2025-01-08 17:57:22 +0530

    Created all crud operations

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ dotenv.config({});
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGODB_URL;
 
+app.use(express.json());
+
 // 1st dimension is route, 2nd is callback function
 // to get resource from server
 app.get("/", (request, response) => {
@@ -16,7 +18,7 @@ app.get("/", (request, response) => {
 });
 
 // route to create new book
-app.post("/", async (request, response) => {
+app.post("/books", async (request, response) => {
   try {
     if (
       !request.body.title ||
@@ -37,6 +39,73 @@ app.post("/", async (request, response) => {
     const book = await BookModel.create(newBook);
 
     return response.status(201).send(book);
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+// get all books with the help of mongoose
+app.get("/books", async (request, response) => {
+  try {
+    const books = await BookModel.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+// get all books with the help of mongoose
+app.get("/books/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const book = await BookModel.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+// update book by id
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response
+        .status(400)
+        .send({ message: "Please fill all required fields" });
+    }
+
+    const id = request.params.id;
+
+    const result = await BookModel.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(400).send({ message: "Book Not Found" });
+    }
+
+    return response.status(200).send({ message: "Book Updated Successfully" });
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+// Delete book by id
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const result = await BookModel.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).send({ message: "Book Not Found" });
+    }
+
+    return response.status(200).send({ messge: "Book Deleted Successfully" });
   } catch (error) {
     return response.status(500).send({ message: error.message });
   }
